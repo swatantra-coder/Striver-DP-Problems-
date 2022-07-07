1c61db8db353e239045ae72ae09f6e3c9c6476be
Swatantra Singh <68724074+swatantra-coder@users.noreply.github.com>
2022-07-07 18:09:53 +0530

    Update MinimumPathSum.cpp

--- MinimumPathSum.cpp
+++ MinimumPathSum.cpp
@@ -25,3 +25,41 @@ int minPathSum(vector<vector<int>>& grid)
         
         
     }
+
+
+
+// <h1>Space optimized code---------</h1>
+
+
+
+int minSumPath(vector<vector<int>> &grid) 
+{
+    int n = grid.size();
+    int m = grid[0].size();
+    vector<int> prev(m,0);
+    for(int i=0;i<n;i++)
+    {
+        vector<int> temp(m,0);
+        for(int j=0;j<m;j++)
+        {
+            if(i==0 and j==0)
+                temp[j]=grid[i][j];
+            else
+            {
+                int left=grid[i][j];
+                int up=grid[i][j];
+                if(i>0)
+                    up+=prev[j];
+                else
+                    up=1e9;
+                if(j>0)
+                    left+=temp[j-1];
+                else
+                    left=1e9;
+                temp[j]=min(left,up);
+            }
+        }
+        prev=temp;
+    }
+    return prev[m-1];
+}
